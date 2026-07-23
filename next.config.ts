ae49589aa020e457771d92ca3a689ee38767df3a
Amrin <sorcheatamrin954@gmail.com>
2026-07-23 08:12:37 +0700

    update nextcgf

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+    // @ts-ignore
     allowedDevOrigins: ['192.168.72.1'],
   },
 };
